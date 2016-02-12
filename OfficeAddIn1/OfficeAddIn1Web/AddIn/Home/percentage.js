9f80fa02d68f7f107c06b83d8b09ca34fcffb5be
Eric Moloney <moloneer@tcd.ie>
2016-02-12 01:29:19 +0000

    added percentage completion functionality and testing

---
+++ OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/percentage.js
@@ -0,0 +1,28 @@
+﻿
+function getNumberOfWords() {
+    Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+        function (result) {
+            
+            var wordCount = result.value.split(" ").length;
+            console.log("Wordcount " + wordCount);
+            calculatePercentage(wordCount);
+
+        }
+    );
+
+}
+
+function calculatePercentage(wordCount) {
+
+    var wordCountAim = document.getElementById("word-aim").value;
+    var percentage = (wordCount / wordCountAim) * 100;
+    displayPercentage(percentage);
+
+
+}
+
+function displayPercentage(p) {
+
+    document.getElementById("percentage").innerHTML = p + "%" + " completed!";
+
+}
