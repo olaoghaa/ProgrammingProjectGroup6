b83d389fc4c733bfca9b1d2dc4cd31c7d77dee2e
Eoghan Quinn <QUINNE16@tcd.ie>
2016-02-27 20:05:37 +0000

    ui shit

--- OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/Home.js
+++ OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/Home.js
@@ -3,10 +3,12 @@
 (function () {
     "use strict";
 
+
     // The initialize function must be run each time a new page is loaded
     Office.initialize = function (reason) {
         $(document).ready(function () {
             //$('#percent-calculate').click(getNumberOfWords);
+           
         });
     };
 
