a55d421b2eac25ed1a53e57ef60277a7984fc538
dariatsar <tsaregod@tcd.ie>
2016-02-29 09:16:09 +0000

    fixed input structure

--- OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/EssayStructure.js
+++ OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/EssayStructure.js
@@ -1,6 +1,30 @@
 ﻿function getStructureInput() {
 
     var value = document.getElementById('Structure').value;
-    Office.context.document.setSelectedDataAsync(value); 
-
+   
+    Office.context.document.setSelectedDataAsync(value);
+    
+    Word.run(function (context) {       
+        var selection = context.document.getSelection();
+        selection.font.size = 16;        
+        return context.sync().then(function () {
+            var paragraphs = selection.paragraphs;
+            context.load(paragraphs, 'style');
+            return context.sync().then(function () {
+                for (var i = 0; i < paragraphs.items.length; i++) {
+                    var paragraph = paragraphs.items[i];
+                    paragraph.insertBreak('page', 'After');
+                }                
+                return context.sync().then(function () {
+                    console.log('Inserted text at the end of the first paragraph.');
+                });
+            })
+        });
+    })
+    .catch(function (error) {
+        console.log('Error: ' + JSON.stringify(error));
+        if (error instanceof OfficeExtension.Error) {
+            console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+        }
+    });
 }
