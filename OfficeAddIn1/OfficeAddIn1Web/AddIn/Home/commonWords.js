242f34fb4161fc839cb1b43d89b28f35db5daf20
AODAN O Laoghaire <olaoghaa@tcd.ie>
2016-03-01 20:31:20 +0000

    Putting back in the right algorithm and filter

--- OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/commonWords.js
+++ OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/commonWords.js
@@ -9,10 +9,20 @@ function wordSize(name, value) {
 function getMostCommonWords() {
     Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
         function (result) {
+            var list = document.getElementById('words');
+            var filter1 = list.innerHTML;
+            var filter = filter1.split(",");
+            var listMax = filter.length;
             var count = 0;
             var i = 0;
-            var wordCount = result.value.split(" ");
-            var max = result.value.split(" ").length;
+            var xx = result;
+            var separators = [' ', '\\\ ', '-', '\\\(', '\\\)', '\\. ', '/', "\r\n", '\\\?'];
+            console.log(separators.join('|'));
+            var wordCount = result.value.split(new RegExp(separators.join('|'), 'g'));
+            console.log(wordCount)
+            //var wordCount = result.value.split(" ");
+            // var max = result.value.split(" ").length;
+            var max = wordCount.length;
             var dict = [max];
             var county = new Array(max);
             var counter = 0;
@@ -22,7 +32,7 @@ function getMostCommonWords() {
             }
             var p = 0;
             while (p < max) {
-                dict[p].word = wordCount[p];
+                dict[p].word = wordCount[p].toLowerCase();
                 dict[p].value = 1;
                 p++;
             }
@@ -55,8 +65,18 @@ function getMostCommonWords() {
                 q++;
         }
 
-
-            bubbleSort(dict);
+            count = max - 1;
+            while (count >= 0) {
+                   var you = 0;
+                   while (you < listMax) {
+                           if (dict[count].word == filter[you]) {
+                                  dict[count].value = 0;
+                            }
+                            you++;
+                   }
+                   count--;
+            }
+            selectionSort(dict);
             displayCommmonWords(dict, max-1);
         }
     );
@@ -80,20 +100,41 @@ function bubbleSort(arr) {
     return arr;
 }
 
+function selectionSort(arr) {
+       var minIdx, temp,
+            len = arr.length;
+        for (var i = 0; i < len; i++) {
+                minIdx = i;
+                for (var j = i + 1; j < len; j++) {
+                        if (arr[j].value < arr[minIdx].value) {
+                                minIdx = j;
+            }
+        }
+                temp = arr[i].value;
+                tmp = arr[i].word;
+                arr[i].value = arr[minIdx].value;
+                arr[i].word = arr[minIdx].word;
+                arr[minIdx].value = temp;
+                arr[minIdx].word = tmp;
+    }
+    return arr;
+}
+
 
 function displayCommmonWords(t, c) {
     var i = 0;
-    while (i < t.length) {
-
+    if (c >= 0 && t[c].value != "") {
         document.getElementById("common").innerHTML = t[c].word + " is used " + t[c].value + " time(s)";
-        document.getElementById("common1").innerHTML = t[c-1].word + " is used " + t[c-1].value + " time(s)";
-        document.getElementById("common2").innerHTML = t[c-2].word + " is used " + t[c-2].value + " time(s)";
-        document.getElementById("common3").innerHTML = t[c-3].word + " is used " + t[c-3].value + " time(s)";
-       
-
-        i++;
     }
-
+       if (c - 1 >= 0 && t[c - 1].value != "") {
+                document.getElementById("common1").innerHTML = t[c - 1].word + " is used " + t[c - 1].value + " time(s)";
+            }
+        if (c - 2 >= 0 && t[c - 2].value != "") {
+                document.getElementById("common2").innerHTML = t[c - 2].word + " is used " + t[c - 2].value + " time(s)";
+            }
+        if (c - 3 >= 0 && t[c - 3].value != "") {
+                document.getElementById("common3").innerHTML = t[c - 3].word + " is used " + t[c - 3].value + " time(s)";
+            }
 
 }
 
