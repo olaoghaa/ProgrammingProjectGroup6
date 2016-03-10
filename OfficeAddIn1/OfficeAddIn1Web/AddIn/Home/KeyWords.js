44306f1ab4e9d43d5af7f75a4f4beb7f72d8d105
Eoghan Quinn <QUINNE16@tcd.ie>
2016-03-10 01:12:34 +0000

    started reworking keyword output

--- OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/KeyWords.js
+++ OfficeAddIn1/OfficeAddIn1Web/AddIn/Home/KeyWords.js
@@ -1,10 +1,10 @@
 ﻿var keywords = [];
-
+var numKeywords = [];
 function addKeywords() {
 
     var value = document.getElementById('keywords').value;
     var keywordArray = value.split(",");
-    
+
     if (keywords.length === 0) {
         for (var j = 0; j < keywordArray.length; j++) {
             keywords.push(keywordArray[j]);
@@ -19,64 +19,88 @@ function addKeywords() {
         }
         keywords = keywords.concat(keywordArray);
     }
-    
+
+
+    var table = document.getElementById('key-table');
+
+    ('key-table tr').remove();
+
+    for (var i = 0; i < keywords.length; i++) {
+        document.getElementById(key).innerHTML = keywords[i];
+
+        var row = table.insertRow(i);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+    }
+
+
+
     displayKeywords();
 }
 
-
-function displayKeywords() {           
-     document.getElementById("keyword").innerHTML = keywords;   
-}
-
-
 function calculateKeywords() {
-    var numKeywords = [];
+
     for (var i = 0; i < keywords.length; i++) {
         numKeywords.push(0);
     }
-    console.log("yas");    
-           Word.run(function (context) {               
-               var thistext = context.document.body;
-               context.load(thistext);
-               return context.sync().then(function () {                
-                   var text = thistext.text;                   
-                   for (var i = 0; i < keywords.length; i++) {
-                       var re = new RegExp(keywords[i].toString(), 'gi');                       
-                       var num = text.match(re);                      
-                       if (num != null) {
-                           numKeywords[i] = num.length;                           
-                       }
-                   }
-               });
-                                      
-              
-           });
-           displayKeywordFreqs(numKeywords);
-}
-function calc() {
     Word.run(function (context) {
-        var searchResults = context.document.body.search('apple', options);      
-        context.load(searchResults);        
-        return context.sync().then(function () {
-            console.log('Found count: ' + searchResults.items.length);
+        for (var i = 0; i < keywords.length; i++) {
+            var searchResults = context.document.body.search(keywords[i], { ignorePunct: true });
+            context.load(searchResults);
+            return context.sync().then(function () {
+                var len = searchResults.items.length;
+                console.log('Found count: ' + len);
+                if (searchResults != null) {
+                    add(i, len);
+                    numKeywords[i] = len;
+                    console.log(numKeywords[i]);
 
-        });
+
+                }
+
+            });
+        }
+
     })
-.catch(function (error) {
-    console.log('Error: ' + JSON.stringify(error));
-    if (error instanceof OfficeExtension.Error) {
-        console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-    }
-})
+    console.log(numKeywords[0]);
+    displayKeywordFreqs();
+
+}
+
+function add(index, num) {
+    numKeywords[index] = num;
 }
 
 
-function displayKeywordFreqs(freqs) {
+function displayKeywords2() {
+
+    for (var i = 0; i < keywords.length; i++) {
+        var key = 'key';
+        var count = 'kcount';
+        var num = i.toString();
+
+        key = key.concat(num);
+        count = count.concat(num);
+
+        document.getElementById(key).innerHTML = keywords[i];
+        document.getElementById(count).innerHTML = numKeywords[i];
+    }
+
+}
+
+function displayKeywords() {
+    document.getElementById("keyword").innerHTML = keywords;
+}
+
+
+
+function displayKeywordFreqs() {
 
     var keywordfreqs = [];
-    for (var i = 0; i < keywords.length; i++){
+    for (var i = 0; i < keywords.length; i++) {
         keywordfreqs.push(keywords[i] + " ");
-        keywordfreqs.push(freqs[i] + "\n");
+        console.log(numKeywords[i]);
+        keywordfreqs.push(numKeywords[i] + "\n");
 
     }
 
@@ -84,7 +108,7 @@ function displayKeywordFreqs(freqs) {
     table.border = "0";
     var columnCount = keywordfreqs.length;
     var row = table.insertRow(-1);
-    
+
     var headerCell = document.createElement("TH");
     headerCell.innerHTML = "Keywords";
     row.appendChild(headerCell);
@@ -92,7 +116,7 @@ function displayKeywordFreqs(freqs) {
     headerCell.innerHTML = "Times Used";
     row.appendChild(headerCell);
 
-    for (var i = 0; i < keywordfreqs.length; i+=2) {
+    for (var i = 0; i < keywordfreqs.length; i += 2) {
         row = table.insertRow(-1);
         var cell = row.insertCell(-1);
         cell.innerHTML = keywordfreqs[i];
@@ -103,9 +127,7 @@ function displayKeywordFreqs(freqs) {
     var keyTable = document.getElementById("keyTable");
     keyTable.innerHTML = "";
     keyTable.appendChild(table);
-    
 
-    //document.getElementById("keywordFreq").innerHTML = keywordfreqs;   
 
 }
 
